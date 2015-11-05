d7613ed052d67d54f762382dc77980e43ef22647
thattommyhall <thattommyhall@gmail.com>
2015-11-05 23:07:25 +0000

    Can Godelise programs

--- src/urm/core.clj
+++ src/urm/core.clj
@@ -1,13 +1,22 @@
 (ns urm.core)
 
-(defn Inc [register jump-to]
+(defn inc [register jump-to]
+  [:inc register jump-to])
+
+(defn deb [register jump-to branch-on-zero]
+  [:deb register jump-to branch-on-zero])
+
+(defn end []
+  [:end])
+
+(defn inc-fn [register jump-to]
   (fn [{:keys [position registers] :as state}]
     (let [current (get-in state [:registers register] 0)]
       (-> state
-          (assoc-in [:registers register] (inc current))
+          (assoc-in [:registers register] (+ 1 current))
           (assoc :position jump-to)))))
 
-(defn Deb [register jump-to branch-on-zero]
+(defn deb-fn [register jump-to branch-on-zero]
   (fn [{:keys [position registers] :as state}]
     (let [current (get-in state [:registers register] 0)
           branch? (= current 0)]
@@ -15,7 +24,7 @@
           (assoc-in [:registers register] (if branch? current (dec current)))
           (assoc :position (if branch? branch-on-zero jump-to))))))
 
-(defn End [] identity)
+(defn end-fn [] identity)
 
 (defn next-state [{:keys [position program] :as state}]
   (let [next-fn (nth program position)]
@@ -27,7 +36,7 @@
       (get-in state [:registers 0])
       (recur next))))
 
-(def lookup { 'Deb Deb 'Inc Inc 'End End })
+(def lookup { :deb deb-fn :inc inc-fn :end end-fn })
 
 (defn functionise [[instruction & args]]
   (apply (lookup instruction) args))
@@ -41,3 +50,95 @@
 
 (defn eval-urm [statements args]
   (apply (urm->fn statements) args))
+
+(defn inc-by [n]
+  (fn [[instruction register jump-to branch-on-zero :as statement]]
+    (case instruction
+      :deb (deb register (+ jump-to n) (+ branch-on-zero n))
+      :inc (inc register (+ jump-to n))
+      :end (end))))
+
+(defn increment-instruction-numbers-by [urm n]
+  (map (inc-by n) urm))
+
+(defn zero [register]
+  [(deb register 0 1)
+   (end)])
+
+(defn comp-urm
+  ([urm1 urm2 & urms]
+   (comp-urm urm1
+             (apply comp-urm urm2 urms)))
+  ([urm1 urm2]
+   (let [length1 (dec (count urm1))
+         start-of-program (take length1 urm1)
+         rest-of-program (increment-instruction-numbers-by urm2 length1)
+         ]
+     (concat start-of-program
+             rest-of-program))))
+
+(defn pow [a n]
+  (bigint (Math/pow a n)))
+
+(defn divides? [n div]
+  (= 0 (rem n div)))
+
+(defn factors-of-2
+  ([n] (factors-of-2 n 0))
+  ([n so-far]
+   (if (divides? n 2)
+     (recur (/ n 2)
+            (+ so-far 1))
+     so-far)))
+
+(defn <<>> [x y]
+  (* (pow 2 x)
+     (+ (* 2 y) 1)))
+
+(defn un<<>> [n]
+  (let [x (factors-of-2 n)
+        y (/ (dec (/ n (pow 2 x)))
+             2)]
+    [x y]))
+
+(defn <> [x y]
+  (dec (<<>> x y)))
+
+(defn un<> [n]
+  (un<<>> (+ n 1)))
+
+(defn code-list [[h & t :as number-list]]
+  (if (empty? number-list)
+    0
+    (<<>> h (code-list t))))
+
+(defn decode-list [code]
+  (if (= code 0)
+    '()
+    (let [[h code'] (un<<>> code)]
+      (cons h
+            (decode-list code')))))
+
+(defn code-instruction [[instruction register jump-to branch-on-zero]]
+  (case instruction
+    :inc (<<>> (* 2 register) jump-to)
+    :deb (<<>> (+ (* 2 register) 1)
+               (<> jump-to branch-on-zero))
+    :end 0))
+
+(defn decode-instruction [code]
+  (if (= code 0)
+    (end)
+    (let [[y z] (un<<>> code)]
+      (if (even? y)
+        (inc (/ y 2) z)
+        (let [[j k] (un<> z)]
+          (deb (/ (dec y) 2)
+               j
+               k))))))
+
+(defn code-program [instructions]
+  (code-list (map code-instruction instructions)))
+
+(defn decode-program [code]
+  (map decode-instruction (decode-list code)))
--- test/urm/core_test.clj
+++ test/urm/core_test.clj
@@ -3,13 +3,65 @@
             [urm.core :refer :all]))
 
 (expect 2
-        (eval-urm '[(Inc 0 1)
-                    (End)]
+        (eval-urm [(inc 0 1)
+                   (end)]
                   [1]))
 
-(def add (urm->fn '[(Deb 1 1 2)
-                    (Inc 0 0)
-                    (End)]))
+(def add (urm->fn [(deb 1 1 2)
+                   (inc 0 0)
+                   (end)]))
 
 (expect 3
         (add 1 2))
+
+(expect [(deb 0 0 1)
+         (deb 1 1 2)
+         (end)]
+        (comp-urm (zero 0)
+                  (zero 1)))
+
+(expect [(deb 0 0 1)
+         (deb 1 1 2)
+         (deb 2 2 3)
+         (end)]
+        (comp-urm (zero 0)
+                  (zero 1)
+                  (zero 2)))
+
+(expect (<<>> 0 13)
+        (<> 2 3))
+
+(expect 8
+        (code-list [3]))
+
+(expect 34
+        (code-list [1 3]))
+
+(expect 276
+        (code-list [2 1 3]))
+
+(expect 18
+        (code-instruction (deb 0 0 2)))
+
+(expect (end)
+        (decode-instruction 0))
+
+(expect (deb 0 0 2)
+        (decode-instruction 18))
+
+(expect 3
+        (factors-of-2 8))
+
+(expect [0 13]
+        (un<<>> 27))
+
+(expect [2 3]
+        (un<> 27))
+
+(expect 786432
+        (code-program [(deb 0 0 2)
+                       (end)]))
+
+(expect [(deb 0 0 2)
+         (end)]
+        (decode-program 786432))
